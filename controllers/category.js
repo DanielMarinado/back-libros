4d30118e6d75df8a0768cc1a9206340576b47c0e
Daniel Marinado <daniel.marinado@gmail.com>
2022-04-23 22:55:58 -0400

    quita redis

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,6 @@
 const Category = require("../models/category");
 const slugify = require("slugify");
-const { GET_ASYNC, SET_ASYNC } = require("../redis/index");
+// const { GET_ASYNC, SET_ASYNC } = require("../redis/index");
 const { request } = require("express");
 
 exports.create = async ( req, res ) => {
